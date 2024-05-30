2168e00ce63a3cd692250b3d9dfc34fefea5c25e
Haresz <rizalfikri852@gmail.com>
2024-05-30 21:52:59 +0700

    chore: set colors in tailwind.config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    colors: {
+      primary: '#008DDA',
+      secondary: '#41C9E2',
+      tertiary: '#ACE2E1',
+      travertine: '#F7EEDD',
+    },
     extend: {},
   },
   plugins: [],
