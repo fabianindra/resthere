ec0f9e9059617cf0e74653b816cb97f26716b13b
Haresz <rizalfikri852@gmail.com>
2024-06-07 21:01:08 +0700

    style: add section property in landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,11 @@ import LocationBox from '@/components/layout/home/LocationBox';
 import PromoSection from '@/components/layout/home/PromoSection';
 import SearchButton from '@/components/layout/home/SearchButton';
 import UpdateEmail from '@/components/layout/home/UpdateEmail';
-import { Box, HStack } from '@chakra-ui/react';
+import CustomCard from '@/components/ui/CustomCard';
+import { Box, HStack, Heading, VStack, Text, Button } from '@chakra-ui/react';
+import { SealPercent, CaretLeft, CaretRight } from '@phosphor-icons/react';
+import { ArrowRight } from '@phosphor-icons/react/dist/ssr';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Home() {
@@ -31,6 +35,34 @@ export default function Home() {
         </HStack>
         <ClouserDestination />
         <PromoSection />
+
+        <Box mt={20} className="w-full">
+          <VStack alignItems={'start'}>
+            <HStack>
+              {/* <SealPercent size={40} /> */}
+              <Heading as={'h3'} size={'lg'}>
+                Best property
+              </Heading>
+            </HStack>
+            <HStack w={'full'} justifyContent={'space-between'}>
+              <Text color={'gray'} fontSize={'md'}>
+                Find Your Ideal Stay Perfectly Suited To You!
+              </Text>
+              <HStack mr={6}>
+                <Link className=" underline text-sm" href={'/list-room'}>
+                  See more
+                </Link>
+                <ArrowRight size={20} />
+              </HStack>
+            </HStack>
+          </VStack>
+          <HStack my={10} justifyContent={'space-between'}>
+            <CustomCard city={'Jepara'} name={'Sailendra'} price={0} />
+            <CustomCard city={'Jepara'} name={'Sailendra'} price={0} />
+            <CustomCard city={'Jepara'} name={'Sailendra'} price={0} />
+            <CustomCard city={'Jepara'} name={'Sailendra'} price={0} />
+          </HStack>
+        </Box>
       </Box>
       <UpdateEmail />
     </Box>
