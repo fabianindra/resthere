421b9b23e65e9a0471ee4db2934c005831e2d088
fabianindra <fabianindra@gmail.com>
2024-06-19 14:33:50 +0700

    refactor: minor edit

--- apps/web/src/components/layout/property-detail/ModalDeleteRoom.tsx
+++ apps/web/src/components/layout/property-detail/ModalDeleteRoom.tsx
@@ -61,7 +61,7 @@ export default function ModalDeleteRoom({ id }: { id: number }) {
             </AlertDialogHeader>
 
             <AlertDialogBody>
-              Are you sure? You can't undo this action afterwards.
+              Are you sure? You cant undo this action afterwards.
             </AlertDialogBody>
 
             <AlertDialogFooter>
